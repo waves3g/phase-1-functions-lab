b42f7115b34cb212d0eff8bbc2489e2dcb99be0a
Courtney Garcia <wavesofgrace3@gmail.com>
2022-04-05 14:17:36 -0700

    commit tree 545566fd19952c2198b173cfe8b08b8f4a5b3b65

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Code your solution in this file!
+const headquarters = 42;
+const feet = 264;
+
+function distanceFromHqInBlocks(num){
+    if (num > 42){
+        return num - headquarters;
+    } else if (num < 42){
+        return headquarters - num;
+    }
+    
+  }
+
+function distanceFromHqInFeet(num) {
+  return distanceFromHqInBlocks(num) * feet;
+   // call the distanceFromHqInBlocks function from inside the distanceFromHqInFeet function,
+   // passing the argument from distanceFromHqInFeet into distanceFromHqInBlocks
+
+   // the return value of distanceFromHqInBlocks can then be used to calculate feet
+  }  
+
+function distanceTravelledInFeet(headquarters, num2) {
+    if (num2 > headquarters){
+        return (num2 - headquarters)*feet;
+    } else {
+        return (headquarters - num2)*feet;
+    }     
+    //returns the number of feet traveled
+  }
+
+function calculatesFarePrice(start, destination) {
+    const num2 = Math.abs(start - destination) * 264;
+        if (num2 <= 400) {
+            return 0;
+        } else if (num2 > 400 && num2 <= 2000) {
+            return (num2 - 400) * .02; 
+        } else if (num2 > 2000 && num2 <= 2500) {
+            return 25;
+        } else if (num2 > 2500) {
+            return 'cannot travel that far';
+        }
+    
+  };
+    //returns the fare for the customer
